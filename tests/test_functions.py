ea05cdb925506ed5c53f57274c3d39863fd973ec
TungCheng Chen(OmniEyes) <rondou.chen@theomnieyes.com>
2023-05-06 13:46:04 +0800

    Add copy testing

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,13 +1,38 @@
 from pathlib import Path
 
 import pytest
+import hashlib
+import shutil
 
 def test_write_001(cache):
     p: Path = cache / 'test_write_001.txt'
     with p.open(mode='a') as f:
         f.write('test_write_001')
-    print(cache)
-    assert True
 
-def test_write_002():
-    assert True
+    c: str = ''
+    with p.open(mode='r') as f:
+        c = f.read()
+
+    assert c == 'test_write_001'
+
+def test_write_002(cache):
+    p: Path = cache / 'test_write_002.txt'
+    with p.open(mode='a') as f:
+        f.write('test_write_002')
+
+    to_file = cache / 'test_write_002.txt.bak'
+    shutil.copy(p, to_file)
+
+    m = hashlib.md5()
+    m2 = hashlib.md5()
+    o: str = ''
+    t: str = ''
+    with p.open(mode='rb') as f, to_file.open(mode='rb') as fs:
+        for chunk in iter(lambda: f.read(4096), b''):
+            m.update(chunk)
+        for chunk in iter(lambda: fs.read(4096), b''):
+            m2.update(chunk)
+
+        o = m.hexdigest()
+        t = m2.hexdigest()
+    assert o == t
